bc4be5cf72773020a7cb45bb3ff237e26517a7a1
Kushagra Vikram Singh <singhkushagravikram17@gmail.com>
2018-07-03 01:24:38 +0530

    Supplementary visualizations for project.

--- visuals.py
+++ visuals.py
@@ -1,4 +1,4 @@
-###########################################
+##########################################
 # Suppress matplotlib user warnings
 # Necessary for newer version of matplotlib
 import warnings
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
 # Display inline matplotlib plots with IPython
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
-###########################################
+##########################################
 
 import matplotlib.pyplot as pl
 import matplotlib.patches as mpatches
